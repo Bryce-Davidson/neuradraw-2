df4ea7d3f11dafc4bda78b005b46a9e90d7508ad
Bryce <bryce_davidson@icloud.com>
2020-08-16 19:38:01 -0700

    Working, fps controller

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import Scene from './Controllers/Scene';
 // import DNN from './Assets/Custom/Basic/DNN-Basic';
 import DNN from './Assets/Custom/Controlled/DNN-Controlled';
 
-// var s1 = new Scene(ctx, "s1", {
-//     duration:1000, 
-//     show_frame_count:true,
-//     fps: 60
-// })
+var s1 = new Scene(ctx, "s1", {
+    duration:1000, 
+    show_frame_count:true,
+    fps: 60
+})
 
-// var d1 = new DNN("d1", 0, s1.num_frames, {
-//     x: 0,
-//     y: 0,
-//     diameter: 60,
-//     layer_spacing: 120,
-//     node_spacing: 60,
-//     weight_colors: 'black',
-//     weight_thicknesses: 1.3
-// })
+var d1 = new DNN("d1", 0, s1.num_frames, {
+    x: 0,
+    y: 0,
+    diameter: 60,
+    layer_spacing: 120,
+    node_spacing: 60,
+    weight_colors: 'black',
+    weight_thicknesses: 1.3
+})
 
-// d1.add_layer(2, "blue", "input", {})
-// d1.add_layer(3, "green", "h_1", {})
-// d1.add_layer(2, "red", "output", {})
+d1.add_layer(2, "blue", "input", {})
+d1.add_layer(3, "green", "h_1", {})
+d1.add_layer(2, "red", "output", {})
 
-// s1.add_asset(d1)
-// s1.play();
+s1.add_asset(d1)
+s1.play();
